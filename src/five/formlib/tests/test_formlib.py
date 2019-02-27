6e31d3b1d6010f55d95cf2ffb89e35add7ecab05
dieter <dieter@handshake.de>
2019-02-27 10:23:14 +0100

    addressing change requests for PR #7

--- src/five/formlib/tests/test_formlib.py
+++ src/five/formlib/tests/test_formlib.py
@@ -71,7 +71,7 @@ def http_request(url, form_parts=None, body=None, auth=None):
     The primary purpose of this auxiliary function is to compute
     the `Content-Length` header. While Python 2's `cgi` module
     seems resilient to a wrong `Content-Length`, Python 3's `cgi` fails
-    badly (at least for a two small value).
+    badly (at least for a too small value).
 
     If given, *form_parts* must be an iterable yielding pairs *name*,*value*.
     *body* is then computed as a typical form response from it.
