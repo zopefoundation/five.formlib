5ce02777843e961dcb0a8c708e6e706978c70310
Jens Vagelpohl <jens@plyp.com>
2023-02-01 11:21:19 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='five.formlib',
-    version='3.0',
+    version='3.1.dev0',
     url='https://github.com/zopefoundation/five.formlib',
     license='ZPL 2.1',
     description='zope.formlib integration for Zope.',
